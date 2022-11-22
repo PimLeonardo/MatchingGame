763a70688642baa00ad53b3cb1ecfe911a3cd9d1
Leonardo Pimentel <leozito4532@gmail.com>
2022-11-21 23:00:55 -0300

    CardBack add

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,9 +1,9 @@
 import "./styles.css";
 
-function CardGame() {
+function CardGame(icon = "icon", alt = "Logo do app") {
   return /*html*/ `
     <article class="card-game">
-        <img src="./images/icon.svg" />
+        <img src="./images/${icon}.svg" alt="${alt}"/>
     </article>
     `;
 }
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,9 +1,13 @@
+import "./styles.css";
+import PlayerName from "../../../src/components/PlayerName";
+
 function ScoreBoard() {
   return /*html*/ `
-  <header>
-  
-  </header>
-  `;
+    <header class="score-board">
+        ${PlayerName("1")}
+        ${PlayerName("2")}
+    </header>
+`;
 }
 
 export default ScoreBoard;
