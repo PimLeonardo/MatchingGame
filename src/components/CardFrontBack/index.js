9e5efacab10c330cd4234f140e1aeb4088af5f8d
Leonardo Pimentel <leozito4532@gmail.com>
2022-11-22 22:21:34 -0300

    Card flip click

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,13 +2,21 @@ import CardGame from "../CardGame";
 import "./styles.css";
 
 function CardFrontBack() {
+  window.cardFrontBack = {};
+  window.cardFrontBack.handleClick = (event) => {
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest(".card-front-back");
+
+    $cardFrontBack.classList.toggle("-active");
+  };
+
   return /*html*/ `
-    <article class="card-front-back">
+    <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
       <div class="card -front">
         ${CardGame()}
       </div>
       <div class="card -back">
-        ${CardGame("botafogo", "Logo do botafogo")}
+        ${CardGame("botafogo", "Logo do Botafogo")}
       </div>
     </article>
   `;
