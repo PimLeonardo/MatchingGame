67ec2f2cb3d1a084001ceaa5cc307bb6b02c309c
Leonardo Pimentel <leozito4532@gmail.com>
2022-11-25 16:56:26 -0300

    layout score board

---
+++ src/components/VsPlayer/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function VsPlayer(content = "VS") {
+  return /*html*/ `
+    <span class="vs-player">
+        ${content}
+    </span>
+  `;
+}
+
+export default VsPlayer;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,15 @@
 import PlayerName from "../../../src/components/PlayerName";
+import VsPlayer from "../../components/VsPlayer";
+import PlayerScore from "../../components/PlayerScore";
 import "./styles.css";
 
 function ScoreBoard() {
   return /*html*/ `
     <header class="score-board">
         ${PlayerName("1")}
+        ${PlayerScore(2)}
+        ${VsPlayer()}
+        ${PlayerScore(1)}
         ${PlayerName("2")}
     </header>
 `;
