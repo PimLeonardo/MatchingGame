9751947cccb220b0311df487b6758cfb3eba61fc
Leonardo Pimentel <leozito4532@gmail.com>
2023-01-26 10:59:53 -0300

    changed img type

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 
 function ArrowDown(currentPlayer = 1) {
   return /*html*/ `
-      <img class="arrow-down" data-currentPlayer="${currentPlayer}" src="images/arrow.svg" alt="Icone de uma seta indicando a vez do jogador" />
+      <img class="arrow-down" data-currentPlayer="${currentPlayer}" src="images/arrow.png" alt="Icone de uma seta indicando a vez do jogador" />
     `;
 }
 
