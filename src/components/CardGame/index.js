aa65038ccaf90553cf11db72224fa2e59be7773a
Leonardo Pimentel <leozito4532@gmail.com>
2023-01-26 12:18:08 -0300

    refactoring img directory

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 function CardGame(icon = "icon", alt = "Logo do app") {
   return /*html*/ `
     <article class="card-game">
-        <img src="./public/${icon}.png" alt="${alt}"/>
+        <img src="./${icon}.png" alt="${alt}"/>
     </article>
     `;
 }
